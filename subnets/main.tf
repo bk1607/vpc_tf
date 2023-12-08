9105e11ff656eb84b20dbf41f8a833a88b1f7309
bk1607 <baluanumala16@gmail.com>
2023-12-08 19:01:19 +0530

    creating routetables association

--- subnets/main.tf
+++ subnets/main.tf
@@ -33,6 +33,6 @@ resource "aws_route_table" "example" {
 # associate route tables to respective subnets
 resource "aws_route_table_association" "a" {
   for_each = var.subnets
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.example.id
+  subnet_id      = aws_subnet.main[each.value["name"]].id
+  route_table_id = aws_route_table.example[each.value["name"]].id
 }
