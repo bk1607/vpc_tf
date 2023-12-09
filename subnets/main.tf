82297bc106d53e1b57d9c78ecf91b778cc02960c
bk1607 <baluanumala16@gmail.com>
2023-12-09 15:58:37 +0530

    creating ngw and attching them to subnets

--- subnets/main.tf
+++ subnets/main.tf
@@ -76,7 +76,7 @@ resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.main.id
   for_each = var.private_subnets
 
-  route = {
+  route  {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateways[each.value["az"]].id
   }
