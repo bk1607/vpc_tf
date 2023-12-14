5526543b71dba87b0fb4f977e0477ab6af2650de
bk1607 <baluanumala16@gmail.com>
2023-12-14 09:56:59 +0530

    add peer route to dafault route table

--- subnets/main.tf
+++ subnets/main.tf
@@ -147,9 +147,9 @@ resource "aws_vpc_peering_connection" "vpc_peering" {
 # adding peer route in default route table
 resource "aws_route" "r" {
   route_table_id            = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  destination_cidr_block    = aws_vpc.main.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
-  
+
 }
 ##output block for nat gateway
 #output "ngw_ids_by_az" {
