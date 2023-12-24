6156a9b91cb1995d4588d21d6b3a5ae577fd6795
bk1607 <baluanumala16@gmail.com>
2023-12-24 17:36:34 +0530

    getting subnet values

--- data.tf
+++ data.tf
@@ -29,7 +29,7 @@ data "aws_subnets" "vpc_subnets" {
 
   filter {
     name   = "tag:Name"
-    values = ["db_private_subnet_1","db_private_subnet_1"]
+    values = ["db_private_subnet_1","db_private_subnet_2"]
   }
 
   # Add more filters or adjust as needed
