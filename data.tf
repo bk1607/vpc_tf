e381a9f77c2229410a8a33e3835f164dda073938
bk1607 <baluanumala16@gmail.com>
2024-01-06 02:06:11 +0530

    debugging

--- data.tf
+++ data.tf
@@ -2,37 +2,17 @@ data "aws_vpc" "default"{
   default = true
 }
 
+data "aws_subnets" "example" {
 
-#data "aws_vpc" "main" {
-#  tags = {
-#    Name = "main"
-#  }
-#}
+}
 
-#data "aws_subnets" "example" {
-#  filter {
-#    name   = "vpc-id"
-#    values = [data.aws_vpc.main.id]
-#  }
-#  filter {
-#    name   = "db-subnet"
-#    values = [data.a]
-#  }
-#}
-#
 #data "aws_subnet" "example" {
 #  for_each = toset(data.aws_subnets.example.ids)
 #  id       = each.value
 #}
-
-#data "aws_subnets" "vpc_subnets" {
 #
-#  filter {
-#    name   = "tag:Name"
-#    values = ["db_private_subnet_1","db_private_subnet_2"]
-#  }
-#
-#  # Add more filters or adjust as needed
+#output "subnet_cidr_blocks" {
+#  value = [for s in data.aws_subnet.example : s.cidr_block]
 #}
 
 
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "vpc_id" {
-  value = aws_vpc
+output "subnets" {
+  value = data.aws_subnets.example
 }
