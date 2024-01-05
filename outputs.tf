3916b8105780b8cde048ac457c39504535452d9a
bk1607 <baluanumala16@gmail.com>
2024-01-06 03:07:59 +0530

    debugging

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "subnet_cidr_blocks" {
-  #value = { for s in data.aws_subnet.example : s.tags["Name"] => s.cidr_block}
-  value = { for id in data.aws_subnets.example : id.tags["Name"] => id.ids }
+  value = { for s in data.aws_subnet.example : s.tags["Name"] => s.cidr_block}
+  
 }
