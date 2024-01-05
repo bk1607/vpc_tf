54f1d743ad2ffdf018932b71050de8d3da453988
bk1607 <baluanumala16@gmail.com>
2024-01-06 02:14:18 +0530

    debugging

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "subnet_cidr_blocks" {
-  value = [for s in data.aws_subnet.example : s.cidr_block]
+  #value = [for s in data.aws_subnet.example : s.cidr_block]
+  value = data.aws_subnet.example
 }
