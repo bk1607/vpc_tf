f7cccb8a11d87b1f79afe138a28421378a204225
bk1607 <baluanumala16@gmail.com>
2024-01-06 02:27:14 +0530

    debugging

--- data.tf
+++ data.tf
@@ -1,9 +1,14 @@
-data "aws_vpc" "default"{
-  default = true
+data "aws_vpc" "main"{
+  tags = {
+    Name = "main"
+  }
 }
 
 data "aws_subnets" "example" {
-
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
 
 }
 
