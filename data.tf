c17ffe756da4a7f48025795efa2c3e958015bb41
bk1607 <baluanumala16@gmail.com>
2024-01-07 10:03:55 +0530

    adding dependency for data block

--- data.tf
+++ data.tf
@@ -13,13 +13,13 @@ data "aws_subnets" "example" {
     name   = "vpc-id"
     values = [data.aws_vpc.main.id]
   }
-
+  depends_on = [aws_subnet.pub_sub,aws_subnet.pri_sub]
 }
 
 data "aws_subnet" "example" {
   for_each = toset(data.aws_subnets.example.ids)
   id       = each.value
-  depends_on = [aws_subnet.pub_sub,aws_subnet.pri_sub]
+  depends_on = [data.aws_subnets.example]
 }
 
 
