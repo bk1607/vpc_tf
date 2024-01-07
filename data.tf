6d61a87fd76dc03a358a8bc736989b5aa4b72592
bk1607 <baluanumala16@gmail.com>
2024-01-07 10:13:57 +0530

    adding dependency for data block

--- data.tf
+++ data.tf
@@ -1,26 +1,6 @@
 data "aws_vpc" "default" {
   default = true
 }
-data "aws_vpc" "main"{
-  tags = {
-    Name = "main"
-  }
-  depends_on = [aws_vpc.main]
-}
-
-data "aws_subnets" "example" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.main.id]
-  }
-  depends_on = [aws_subnet.pub_sub,aws_subnet.pri_sub]
-}
-
-data "aws_subnet" "example" {
-  for_each = toset(data.aws_subnets.example.ids)
-  id       = each.value
-  depends_on = [data.aws_subnets.example]
-}
 
 
 
