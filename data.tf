5b5cfa89b53da0b1d121fcf823c2743ee5164bf1
bk1607 <baluanumala16@gmail.com>
2024-01-13 10:14:57 +0530

    vpc data

--- data.tf
+++ data.tf
@@ -2,11 +2,11 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_vpc" "vpc_id" {
-  tags = {
-    Name = "main"
-  }
-}
+#data "aws_vpc" "vpc_id" {
+#  tags = {
+#    Name = "main"
+#  }
+#}
 
 
 
--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "public_subnets" {
   value = aws_subnet.pub_sub
 }
 output "vpc_id" {
-  value = data.aws_vpc.vpc_id.id
+  value = aws_vpc.main.id
 }
